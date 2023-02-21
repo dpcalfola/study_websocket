82814a5b7efb4675ecc7446aa366c474475750e9
Fola Flor <dpcalfola@gmail.com>
2023-02-22 08:55:22 +0900

    Feat > Build WebSocket connection
         - Install pip package "channels"
            channels>=3.0.0,<4.0.0i
            (channels upper 4 version was not working)
    
            * Insert this code on settings.py
                ASGI_APPLICATION = 'config.asgi.application'
    
         - Result
            WebSocket connection was built
            In the console.log,
            there's the data received from the server
    
            It is not response by client request
            But server sent data to client for initial connection
            (By chat/consumers.py)
    
        - The flow from server to client with websocket
            Code flow
                1. chat/consumers.py
                    1.1. connect.accept()
                    1.2. connect.send(text_data)
                2. chat/routing.py
                    2.1. Bind WebSocket url pattern with chat/consumers.py
                3. config/asgi.py
                    3.1. Bind URL Router with chat.routing.websocket_urlpatterns
    
            When server starts
                ASGI works on config/asgi.py
                URL Router works on chat/routing.py
                WebSocket url pattern works on chat/consumers.py
                connect.accept() works on chat/consumers.py
                connect.send(text_data) works on chat/consumers.py
                WebSocket connection is built
                Server sends data to client
                Client receives data from server
                Client shows data on console.log
    
    < task.txt >
    
    6. Channels
        6.1 Install package "channels"
            6.1.1 add channels to requirements.txt
                6.1.1.1 WARNING: channels version should be blow 4.0.0 (I used 3.0.5)
            6.1.2 "pip install -r requirements.txt"
        6.2 On settings.py
            6.2.1 Add "channels" to INSTALLED_APPS
            6.2.2 Add "ASGI_APPLICATION = 'config.asgi.application'"
        6.3 Creating routing configuration
            6.3.1 On config/asgi.py
                6.3.1.1 Import ProtocolTypeRouter
                6.3.1.2 Set ProtocolTypeRouter on application
        6.4 Confirm ASGI/Channels working
            6.4.1 Run server - Checkup message of ASGI/Channels
            It should show "Starting ASGI/Channels version 3.0.5 development server at..."
    
    7. Make javascript on lobby.html
        7.1 Create WebSocket object
        7.2 Parse json data and print on console.log
            7.2.1 It would Fail but could see the message of WebSocket connection
    
    8. chat/consumers.py
        8.1 consumer.py is the channel's version of Django view.py
        8.2 Create consumer.py
            8.2.1 import json, WebsocketConsumer
            8.2.2 Create ChatConsumer class
                8.2.2.1 Create connect method includes self.accept(), self.send()
    
    9. Create chat/routing.py
        9.1 import re_path, consumers(chat/consumers.py)
        9.2 Create websocket_urlpatterns
            9.2.1 re_path: ws url, ChatConsumer.as_asgi()
    
    10. on config/asgi.py
        10.1 import URLRouter, AuthMiddlewareStack
            10.1.1 'AuthMiddleware' requires 'SessionMiddleware' to function,
                    which itself requires 'CookieMiddleware'.
                    For convenience, these are also provided
                    as a combined callable called 'AuthMiddlewareStack'
                    that includes all three.
        10.2 Set websocket_urlpatterns on application
            10.2.1 import chat.routing
            10.2.2 add 'websocket': AuthMiddlewareStack(...) in application
        10.3 From now, Do initial migrate and restart server
            10.3.1 Because the django session framework need a database now
            10.3.2 Commands
                10.3.2.1 "python manage.py migrate"
                10.3.2.2 "python manage.py runserver"
            10.3.3 Checkup message of ASGI/Channels
                10.3.3.1 On console.log, There's json data
                        which received from ChatConsumer.connect.send(text_data)
            10.3.4 This was not a response that it get back from the server
                    but rather a message that it sent to itself
                    (initialized by the server)

--- ws_proj_01/config/asgi.py
+++ ws_proj_01/config/asgi.py
@@ -11,6 +11,20 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+# Channel
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+# app
+import chat.routing
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
+})
