d534e52446c6d21dfbf49cf32d4e18c1c38fc748
Fola Flor <dpcalfola@gmail.com>
2023-02-23 01:51:32 +0900

    Feat > Display the message on all clients
        * Result:
            The message is shown for all clients
    
        * Channel Layer grouping
            when the client connects to the server,
    
        * Principle:
            connect(self)
                When the websocket connection is established,
                The client is added to groups
    
            receive(self, text_data) -> chat_message(self, event)
                When the client sends a message to the server,
                The server sends the message to all clients in the group
    
            lobby.html
                Client show the message on HTML
    
        * chore:
            Message does not show on the python console
    
    < task.txt >
    13.
        13.1 On Setting.py
            13.1.1 CHANNEL_LAYER = {...}
        13.2 On chat/consumers.py
            13.1 On connect method
                13.1.1 This method is called When client connect to server
                13.1.2 <async> Grouping clients to one group 'test' when they connect websocket
            13.2. On receive method
                13.2.1 This method is called When server get a text data
                13.2.2 When server get a text data, <async> self.channel_layer.group_send() Throws a data
                        to all clients in group 'test' with {'type': 'chat_message'}
            13.3 On chat_message method
                13.3.1 This method is called when <async> call the group_send method
        13.3 Flow
            13.3.1 Client connect to server (Open WebSocket)
            13.3.2 Server get a text data from client
            13.3.3 Server receive() throws the data to the group
            13.3.4 Server get the data
            13.3.5 Server channel_layer.group_send() with {'type': 'chat_message'} triggers chat_message()
            13.3.6 Server chat_message() sends the text data to all clients in group
            13.3.7 Client(s) get the text data
            13.3.8 Client(s) render a message from the data on HTML

--- ws_proj_01/chat/consumers.py
+++ ws_proj_01/chat/consumers.py
@@ -1,39 +1,58 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 
 # This is the consumer class that will handle the websocket connection
 class ChatConsumer(WebsocketConsumer):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_group_name = None
+
     def connect(self):
         """
-        This method is called when the websocket is handshaking as part of initial connection.
+        This method is called when the websocket is handshaking as part of initial connection
         :return:
         """
 
-        # This will accept the connection
+        # User grouping point
+        self.room_group_name = 'group_name'
+
+        # Now, All the user throw the same group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            # self.Channel_name => Automatically generated channel name for each user
+            self.channel_name
+        )
+        # Accept the connection
         self.accept()
-        # This will send a message back to the client
-        self.send(text_data=json.dumps({
-            'type': 'connection_established',
-            'message': 'You ar now connected!!',
-        }))
 
     def receive(self, text_data=None, bytes_data=None):
         """
-        This method is called when we get a text frame
-        :param text_data: It would be received from the lobby.html String Data looks Like Json
+        This method is called when we get a text data from the websocket
+        :param self:
+        :param text_data: Text data that we get from the websocket
+                that received from the client's javascript
         :param bytes_data:
         :return:
         """
+
         # Convert the text data to json
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
-        # Print the received message to the console
-        print(message)
+        # Send message to room group
+        # group_send() will send the message to all the users in the group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {
+                'type': 'chat_message',
+                'message': message,
+            })
 
-        # Send the message back to the client
+    # This will be called when we call the group_send method
+    def chat_message(self, event):
+        message = event['message']
         self.send(text_data=json.dumps({
             'type': 'chat',
             'message': message,
--- ws_proj_01/config/settings.py
+++ ws_proj_01/config/settings.py
@@ -20,7 +20,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -36,7 +35,6 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -54,6 +52,12 @@ INSTALLED_APPS = [
 ]
 
 ASGI_APPLICATION = 'config.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -85,7 +89,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -95,7 +98,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -115,7 +117,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -126,7 +127,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
