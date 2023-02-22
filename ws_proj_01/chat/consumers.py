03f80a9aa62674e69f7842785e79aa07bdb22754
Fola Flor <dpcalfola@gmail.com>
2023-02-22 22:13:46 +0900

    Feat > Send message from client to server
        Result:
            Print received data on python console Successfully
    
    < task.txt >
    
    11. WebSocket 1 -
        11.0 Goal:
            Sending a message from the client to the server
            and printing the received data on the Python console
        11.1 Check if the WebSocket is open or not
            11.1.1 If the WebSocket is opened, show a message on the Python console:
                WebSocket HANDSHAKING /ws/socket-server/ [127.0.0.1:*****]
                WebSocket CONNECT /ws/socket-server/ [127.0.0.1:*****]
        11.2 On lobby.html:
            11.1.1 Implement the <form> tag for sending text messages
        11.3 In JavaScript:
            11.3.1 Get the <form> tag and create an event listener for 'submit'
            11.3.2 Perform actions in the following order:
                e.preventDefault() -> chatSocket.send() -> form.reset()
        11.4 On chat/consumers.py:
            11.4.1 Create a receive method
                11.4.1.1 When the client sends data to the server, the receive method is called
                11.4.1.2 The receive method receives text_data as a parameter, which could be parsed as JSON data
                11.4.1.3 Print the received data on the Python console Successfully🌟

--- ws_proj_01/chat/consumers.py
+++ ws_proj_01/chat/consumers.py
@@ -2,17 +2,33 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 
 
+# This is the consumer class that will handle the websocket connection
 class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
 
+    def connect(self):
+        """
+        This method is called when the websocket is handshaking as part of initial connection.
+        :return:
+        """
+
+        # This will accept the connection
+        self.accept()
+        # This will send a message back to the client
         self.send(text_data=json.dumps({
             'type': 'connection_established',
             'message': 'You ar now connected!!',
         }))
 
-    # def receive(self, text_data):
-    #     pass
-    #
-    # def disconnect(self, close_code):
-    #     pass
+    def receive(self, text_data=None, bytes_data=None):
+        """
+        This method is called when we get a text frame
+        :param text_data: It would be received from the lobby.html String Data looks Like Json
+        :param bytes_data:
+        :return:
+        """
+        # Convert the text data to json
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # Print the received message to the console
+        print(message)
